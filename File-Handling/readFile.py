c92f8ba21b96b3f72138238745d868eaa3d83e6f
Wambui-123 <yvonne.muthui@student.moringaschool.com>
2022-04-27 08:52:09 +0300

    created intro to flask

---
+++ File-Handling/readFile.py
@@ -0,0 +1,13 @@
+# handle = open("test.txt","r")
+
+# data = handle.read()
+# print(data)
+
+# handle.close()
+
+handle = open("text-write.txt", "w")
+
+handle.write("Hello Moringa")
+handle.close()
+
+f = open("myfile.txt", "w")
---
+++ one.py
@@ -0,0 +1,22 @@
+# Input
+# name=input('What is your name? ')
+# color= input('What is your favourite color? ')
+# print(name +  ' likes ' +  color)
+# Changing datatypes
+# weight_pounds=input('What is your weight in pounds? ')
+# convert=int(weight_pounds)* 0.45
+# print(convert)
+#f means formatted string
+# first_name = 'Yvonne'
+# last_name = 'Muthui'
+# print(f'{first_name} + [{last_name}] is a coder')
+#Length
+# word = "Alphabet"
+# print(len(word))
+#Methods
+# print(word.upper())
+# print(word.find('h'))
+# print(word.replace('Alphabet', 'New-Alphabet'))
+# print('Alpha' in word)
+
+
